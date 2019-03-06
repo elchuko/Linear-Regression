af3e9868cfe1919244f1d5a2d39f62c6bddca85e
Sanchez <Jose_antonio_Sanchez@Dellteam.com>
2019-03-06 12:33:35 -0600

    Fixed method matrixMultiplier for vectors

--- linear_regression.py
+++ linear_regression.py
@@ -4,9 +4,9 @@ def createMatrixes(filename):
 	airfoil_file = open(filename, 'r')
 	lines = airfoil_file.readlines()
 	#matrices y vectores
-	matrixA=[[0 for x in range(5)] for y in range(len(lines))]
+	matrixA = [[0 for x in range(5)] for y in range(len(lines))]
 	matrixAT = [[0 for x in range(len(lines))] for y in range(5)]
-	resultVector =[len(lines)]
+	resultVector =[]
 
 	for currentLine in range(len(lines)):
 		temporary = lines[currentLine].split()
@@ -20,33 +20,45 @@ def createMatrixes(filename):
 
 
 def matrixMultiplier(A, BT):
-	print(len(A))
-	print(len(BT))
 	if(type(BT[0]) is not list):
-		print("si entro aqui")
-		if(len(A) != len(BT)):
+		if(len(A[0]) != len(BT)):
 			print("las matrices no se pueden multiplicar")
 			return 0
-
+		else:
+			resultMatrix = [0 for y in range(len(A))]
+			for i in range(len(A)):
+				for j in range(len(BT)):
+					resultMatrix[i] += float(A[i][j]) * float(BT[j])
+					
 	else:
 		if(len(A) != len(BT[0])):
 			print("Las matrices no se pueden multiplicar")
 			return 0
-
-	resultMatrix = [[0 for x in range(len(BT))] for y in range(len(A[0]))]
-	for i in range(len(BT)):
-   		for j in range(len(A[0])):
-   			for k in range(len(A)):
-   				resultMatrix[i][j] += float(BT[i][k]) * float(A[k][j])
+		else:
+			resultMatrix = [[0 for x in range(len(BT))] for y in range(len(A[0]))]
+			for i in range(len(BT)):
+				for j in range(len(A[0])):
+					for k in range(len(A)):
+						resultMatrix[i][j] += float(BT[i][k]) * float(A[k][j])
 
 	return resultMatrix
 
+def joinMatrixes(matrix, result):
+	pass
+	
+
+def gaussJordan(matrix):
+	pass
+
+
+	
 
 if __name__ == "__main__":
 	A, AT, resultVector = createMatrixes("airfoil_train_.csv")
 	resultMatrix = matrixMultiplier(A,AT)
-	print(len(resultVector))
 	finalVector = matrixMultiplier(AT, resultVector)
+	
+	
 
 
 
